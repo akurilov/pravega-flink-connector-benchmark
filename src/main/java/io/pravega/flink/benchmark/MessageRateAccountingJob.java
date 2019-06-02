cbe09a49c25c3575cca3a1f74835621b8b825e69
Andrey Kurilov <andrey.kurilov@emc.com>
2019-06-03 00:09:55 +0300

    save

--- src/main/java/io/pravega/flink/benchmark/MessageRateAccountingJob.java
+++ src/main/java/io/pravega/flink/benchmark/MessageRateAccountingJob.java
@@ -1,29 +1,26 @@
 package io.pravega.flink.benchmark;
 
-import io.pravega.client.stream.EventRead;
 import io.pravega.connectors.flink.FlinkPravegaReader;
 import io.pravega.connectors.flink.PravegaConfig;
 import io.pravega.connectors.flink.serialization.PravegaSerialization;
 import lombok.val;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SampleJob {
+public class MessageRateAccountingJob {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SampleJob.class);
+	private static final Logger LOG = LoggerFactory.getLogger(MessageRateAccountingJob.class);
 	private static final String DEFAULT_SCOPE = "scope0";
 	private static final String DEFAULT_STREAM = "stream0";
 	private static final String STREAM_PARAM = "stream";
+	private static final String JOB_NAME = MessageRateAccountingJob.class.getSimpleName();
 
 	public static void main(final String... args)
 	throws Exception {
-		LOG.info("Starting the sample job...");
+		LOG.info("Starting the " + JOB_NAME + " ...");
 		// initialize the parameter utility tool in order to retrieve input parameters
 		val params = ParameterTool.fromArgs(args);
 		val pravegaConfig = PravegaConfig
@@ -33,21 +30,18 @@ public class SampleJob {
 		val stream = Utils.createStream(pravegaConfig, params.get(STREAM_PARAM, DEFAULT_STREAM));
 		val env = StreamExecutionEnvironment.getExecutionEnvironment();
 		// create the Pravega source to read a stream of text
-		val source = FlinkPravegaReader.builder()
+		val source = FlinkPravegaReader.<byte[]>builder()
 			.withPravegaConfig(pravegaConfig)
+			.withDeserializationSchema(PravegaSerialization.deserializationFor(byte[].class))
 			.forStream(stream)
 			.build();
-		// count each word over a 10 second time period
+		// count each word over a 1 second time period
 		val dataStream = env
 			.addSource(source)
-			.name("Pravega Stream")
-			.timeWindowAll(Time.seconds(10))
-			.process(new ProcessAllWindowFunction<Object, Object, TimeWindow>() {
-				@Override
-				public void process(
-					final Context context, final Iterable<Object> elements, final Collector<Object> out
-				) throws Exception {
-				}
-			});
+			.map(x -> 1L)
+			.timeWindowAll(Time.seconds(1))
+			.reduce(Long::sum);
+		dataStream.print();
+		env.execute(MessageRateAccountingJob.class.getSimpleName());
 	}
 }
