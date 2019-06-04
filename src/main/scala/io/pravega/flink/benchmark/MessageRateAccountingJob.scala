2c2542dfe24976e113dd2b8ff91893534031bd24
Kurilov, Andrey <andrey.kurilov@emc.com>
2019-06-04 11:51:29 +0300

    scala code instead of java

---
+++ src/main/scala/io/pravega/flink/benchmark/MessageRateAccountingJob.scala
@@ -0,0 +1,73 @@
+package io.pravega.flink.benchmark
+
+import java.util.concurrent.TimeUnit
+
+import io.pravega.client.netty.impl.ConnectionFactoryImpl
+import io.pravega.client.stream.{Stream, StreamConfiguration}
+import io.pravega.client.stream.impl.{ControllerImpl, ControllerImplConfig}
+import io.pravega.connectors.flink.{FlinkPravegaReader, PravegaConfig}
+import io.pravega.connectors.flink.serialization.PravegaSerialization
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.slf4j.LoggerFactory
+
+object MessageRateAccountingJob {
+
+	private val JOB_NAME = "MessageRateAccountingJob"
+	private val LOG = LoggerFactory getLogger JOB_NAME
+	private val DEFAULT_SCOPE = "scope0"
+	private val DEFAULT_STREAM = "stream0"
+	private val STREAM_PARAM = "stream"
+
+	@throws[Exception]
+	def main(args: Array[String]) {
+		LOG info("Starting the " + JOB_NAME + " ...")
+		// initialize the parameter utility tool in order to retrieve input parameters
+		val params = ParameterTool fromArgs args
+		val pravegaConfig = PravegaConfig
+			.fromParams(params)
+			.withDefaultScope(DEFAULT_SCOPE)
+		// create the Pravega input stream
+		val controllerConfig = ControllerImplConfig
+			.builder
+			.clientConfig(pravegaConfig.getClientConfig)
+			.build
+		val connectionFactory = new ConnectionFactoryImpl(pravegaConfig.getClientConfig)
+		val controller = new ControllerImpl(controllerConfig, connectionFactory.getInternalExecutor)
+		try {
+			controller
+				.createScope(DEFAULT_SCOPE)
+				.get(10, TimeUnit.SECONDS)
+			val streamConfig = StreamConfiguration.builder.build
+			controller
+				.createStream(DEFAULT_SCOPE, DEFAULT_STREAM, streamConfig)
+				.get(10, TimeUnit.SECONDS)
+		} finally {
+			if (controller != null) {
+				controller.close()
+			}
+		}
+		val env = StreamExecutionEnvironment.getExecutionEnvironment
+		LOG info "5"
+		// create the Pravega source to read a stream of text
+		val source = FlinkPravegaReader
+			.builder[Array[Byte]]
+			.withPravegaConfig(pravegaConfig)
+			.withDeserializationSchema(PravegaSerialization.deserializationFor(classOf[Array[Byte]]))
+			.forStream(Stream.of(DEFAULT_SCOPE, DEFAULT_STREAM))
+			.build
+		LOG info "6"
+		// count each word over a 1 second time period
+		val dataStream = env
+			.addSource(source)
+			.map((x: Array[Byte]) => 1L)
+			.timeWindowAll(Time.seconds(1))
+			.reduce(java.lang.Long.sum)
+		LOG info "7"
+		dataStream.print
+		LOG info "8"
+		env execute JOB_NAME
+		LOG info "9"
+	}
+}
