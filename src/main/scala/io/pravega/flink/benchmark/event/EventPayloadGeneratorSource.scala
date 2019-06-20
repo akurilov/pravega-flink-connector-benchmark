182f991baf59c977a4571004f326f520a82ff10a
kurila <akurilov123@gmail.com>
2019-06-20 14:03:32 +0300

    save

--- src/main/scala/io/pravega/flink/benchmark/event/EventPayloadGeneratorSource.scala
+++ src/main/scala/io/pravega/flink/benchmark/event/EventPayloadGeneratorSource.scala
@@ -7,21 +7,19 @@ import org.apache.flink.streaming.api.functions.source.{RichParallelSourceFuncti
 class EventPayloadGeneratorSource(size: Int, rate: Double)
 extends RichParallelSourceFunction[Array[Byte]] {
 
-	val delayNanos = Long(
-		if(rate > 0) {
-			1e9 / rate
-		} else {
-			0
-		}
-	)
-
 	var cancelled = false
 	
 	override def run(ctx: SourceFunction.SourceContext[Array[Byte]]): Unit = {
 		var nextTimeNanos = 0L
+		val delayNanos =
+			if(rate > 0) {
+				(1e9 / rate).toLong
+			} else {
+				0L
+			}
 		while(!cancelled) {
 			if(nextTimeNanos < System.nanoTime()) {
-				LockSupport.parkNanos(1)
+				LockSupport parkNanos 1L
 			} else {
 				nextTimeNanos = System.nanoTime() + delayNanos
 				ctx.collect(new Array[Byte](size))
